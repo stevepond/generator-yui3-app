2bcfdbb6e0706771829440011f69893a0e689d38
Stephen Pond <spond@yahoo-inc.com>
2014-01-31 11:34:04 -0600

    first commit

---
+++ goo.js
@@ -0,0 +1,11 @@
+'use strict';
+YUI.add('bar', function (Y) {
+    Y.bar = Y.Base.create('bar', Y.Model, [], {
+    },
+    {
+        ATTRS: {
+        }
+    });
+}, '0.0.1', {
+    requires: []
+});
