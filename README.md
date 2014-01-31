3729d89e6f32cefdc264c43dce48401cb19ea7d7
Stephen Pond <spond@yahoo-inc.com>
2014-01-31 13:42:13 -0600

    view, updated README

---
+++ README.md
@@ -0,0 +1,22 @@
+[Yeoman](http://yeoman.io) generator that scaffolds out a YUI3 app-framework App 
+j
+![](http://i.imgur.com/uKTT2Hj.png)
+
+## Features
+
+* scaffold a YUI3 app, using YUI3 built in App Framework and module support
+* support for model, modellist, view, and widget  w/ tests
+
+## Getting Started
+
+- Install: `npm install -g generator-webapp`
+- Run: `yo yui3-app`
+- Run `grunt` for building and `grunt serve` for preview [*](#serve-note)
+
+##Available generators:
+
+- yui3-app
+- yui3-app:model
+- yui3-app:modellist
+- yui3-app:view
+- yui3-app:widget
--- view/index.js
+++ view/index.js
@@ -5,13 +5,16 @@ var yeoman = require('yeoman-generator');
 var ViewGenerator = module.exports = function ViewGenerator(args, options, config) {
   // By calling `NamedBase` here, we get the argument to the subgenerator call
   // as `this.name`.
-  yeoman.generators.NamedBase.apply(this, arguments);
+    yeoman.generators.NamedBase.apply(this, arguments);
 
-  console.log('You called the view subgenerator with the argument ' + this.name + '.');
+    console.log('You called the view subgenerator with the argument ' + this.name + '.');
 };
 
 util.inherits(ViewGenerator, yeoman.generators.NamedBase);
 
 ViewGenerator.prototype.files = function files() {
-  this.copy('somefile.js', 'somefile.js');
+    var filename = 'app/scripts/views/' + this.name + '' + '.js';
+    var filenameTest = 'test/views/' + this.name + '' + '.js';
+    this.template('view', filename);
+    this.template('test', filenameTest);
 };
